28407b1d0a69611421044a5b5ce988c5b5db698f
prashant-130 <149078895+prashant-130@users.noreply.github.com>
2025-03-06 13:21:58 +0530

    added rotes

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,27 @@
 const express= require('express')
+const app = express();
+const port = 5000;
+
+app.use(express.json())
+//req---->client/user/frontend
+//res--->server/backend
+app.get('/abc',(req,res)=>{
+    // res.send('parminder')
+    res.json({
+        success:true,
+        message:"you are successfully login"
+    })
+
+    console.log(req.body)
+})
+app.put('/put',(req,res)=>{
+    // res.send('data')
+    res.json(
+        {
+            done:this,
+            message:"done"
+        }
+    )
+    console.log(req.body)
+})
+app.listen(port)
