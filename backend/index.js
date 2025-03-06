89eb7754d277943689f3b91b714bfd597c8c55eb
prashant-130 <149078895+prashant-130@users.noreply.github.com>
2025-03-06 14:31:02 +0530

    basic middleware

--- backend/index.js
+++ backend/index.js
@@ -5,23 +5,72 @@ const port = 5000;
 app.use(express.json())
 //req---->client/user/frontend
 //res--->server/backend
-app.get('/abc',(req,res)=>{
-    // res.send('parminder')
-    res.json({
-        success:true,
-        message:"you are successfully login"
-    })
+
+// const isLogn = function(req,res,next)
+// {
+//     console.log("user is login")
+//     next();
+// }
+// app.use(isLogn)
+
+// const isValid=function(req,res,next)
+// {
+    
+//     console.log('yes valid user')
+//     next();
+// }
+// app.use(isValid)
+// const isAdmib=function(req,res,next)
+// {
+   
+//     console.log('yes valid Admin')
+//     next();
+// }
+// app.use(isAdmib)
+
+const firstvalid = function(req,res,next){
+    console.log("first user is valid")
+    next();
+}
+app.use(firstvalid)
+
+const secondvalid= function(req,res,next){
+    console.log("second user is valid")
+    next();
+}
+app.use(secondvalid)
+
+const thirdvalid= function(req, res, next){
+    console.log("third user is valid")
+    next();
+}
+
+app.use(thirdvalid)
+
+
+app.get('/',(req,res)=>{
+    res.send('you are successfully login')
+    // res.json({
+    //     success:true,
+    //     message:"you are successfully login"
+    // })
 
     console.log(req.body)
 })
-app.put('/put',(req,res)=>{
-    // res.send('data')
-    res.json(
-        {
-            done:this,
-            message:"done"
-        }
-    )
-    console.log(req.body)
-})
+
+
+
+
+
+
+// app.put('/put',(req,res)=>{
+//     // res.send('data')
+//     res.json(
+//         {
+//             done:this,
+//             message:"done"
+//         }
+//     )
+//     console.log(req.body)
+// })
 app.listen(port)
